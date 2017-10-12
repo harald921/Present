d12594c4f39115e42cff2e70cc97b132abc067a0
harald921@hotmail.com <harald921@hotmail.com>
2017-10-13 00:43:04 +0200

    Minor changes to TileMap2D

--- Assets/Code/TileMap/Tilemap2D.cs
+++ Assets/Code/TileMap/Tilemap2D.cs
@@ -2,33 +2,20 @@ using UnityEngine;
 
 public class TileMap2D : MonoBehaviour
 {
-    enum Sprites
-    {
-        Grass1,
-        Grass2,
-        Grass3,
-        Water,
-    }
-
     const int _spriteCount = 16;  // Sprite sheets are currently limited to 16x16 sprites. Changes need to be made to shader if this is to be changed.
 
     [SerializeField] int _chunkSize        = 64;          // Amount of tiles on one axis of the chunk
 	[SerializeField] int _spriteResolution = 32;          // Resolution of the tile graphics
 
-    Material tileMapMaterial;
 
+    
     // Start, Update
-    void Start ()
+    void Start()
     {
         GetComponent<MeshRenderer>().material.SetTexture("_DataMap", CreateDataTexture(_chunkSize));
         GetComponent<MeshRenderer>().material.SetFloat("_DataMapSize", _chunkSize);
 	}
 
-    // External
-    static Color GetUVColor(Vector2 inSpriteCoords)
-    {
-        return new Color(inSpriteCoords.x / _spriteCount, inSpriteCoords.y / _spriteCount, 0, 0);
-    }
 
     // Internal
     Texture2D CreateDataTexture(int inTextureSize)
@@ -39,6 +26,25 @@ public class TileMap2D : MonoBehaviour
         dataTexture.Apply();
 
         return dataTexture;
+    }
+
+    Color GetUVColor(Sprite inSprite)
+    {
+        Vector2 spriteCoords = new Vector2
+        {
+            x = (int)inSprite % _spriteCount,
+            y = Mathf.FloorToInt((int)inSprite / _spriteCount)
+        };
+
+        return new Color(spriteCoords.x / _spriteCount, spriteCoords.y / _spriteCount, 0, 0);
+    }
+
+    enum Sprite
+    {
+        Grass1,
+        Grass2,
+        Grass3,
+        Water,
     }
 }
 
